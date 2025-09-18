b955df7c1198f4a7e5dcf328507c8516c9c90539
Juan Lago <juan@leadsupply.com>
2025-09-18 09:23:48 +0200

    Refactored `LogParser` to use a new JSON extraction method for improved accuracy and performance; updated coding standards and incremented build number.

--- src/App/Model/LogParser.php
+++ src/App/Model/LogParser.php
@@ -24,11 +24,11 @@ class Model_LogParser
     const DATETIME_FORMAT = 'Y-m-d H:i:s';
 
 
-	/**
-	 * Last log.
-	 *
-	 * @var array
-	 */
+    /**
+     * Last log.
+     *
+     * @var array
+     */
     protected static $last_log = [];
 
 
@@ -43,69 +43,63 @@ class Model_LogParser
      * @return array|false
      */
     public static function parseLogEntry(
-    	string $entry,
-		DateTimeZone $current_timezone = null,
-		DateTimeZone $to_timezone = null,
-		string $dateformat = 'DATE_ISO8601',
-        bool $smart_serialization = false
-	) {
+        string       $entry,
+        DateTimeZone $current_timezone = null,
+        DateTimeZone $to_timezone = null,
+        string       $dateformat = 'DATE_ISO8601',
+        bool         $smart_serialization = false
+    )
+    {
 
-    	if (empty($entry))
-    		return false;
+        if (empty($entry))
+            return false;
 
-        if (preg_match(static::LOG_EXPR, $entry, $matches))
-        {
-            // Extract JSON data
-            preg_match_all(
-                '/\{(?:[^{}]|(?R))*\}/m',
-                trim($matches[4]),
-                $data,
-            );
+        if (preg_match(static::LOG_EXPR, $entry, $matches)) {
 
             $structs = [];
+            $initialMatchLength = strlen($matches[4]);
 
-            if (count($data[0])) {
-                foreach ($data[0] as $d) {
-                    $structs[] = trim($d);
-                    $matches[4] = str_replace($d, '', $matches[4]);
-                }
+            // Extract JSON data
+            foreach (static::jsonExtract($matches[4]) as $struct) {
+                $structs[] = $struct['string'];
+                $reduction = $initialMatchLength - strlen($matches[4]);
+                $matches[4] = substr_replace($matches[4], '', $struct['start'] - $reduction, $struct['length']);
             }
 
             $timestamp = Carbon::createFromFormat(static::DATETIME_FORMAT, $matches[1], $current_timezone);
 
             if ($to_timezone)
-            	$timestamp->setTimezone($to_timezone);
+                $timestamp->setTimezone($to_timezone);
 
-            switch ($dateformat)
-			{
-				case 'epoch':
-					$timestamp = time() * 1000;
-					break;
+            switch ($dateformat) {
+                case 'epoch':
+                    $timestamp = time() * 1000;
+                    break;
 
-				case 'timestamp':
-					$timestamp = $timestamp->getTimestamp();
-					break;
+                case 'timestamp':
+                    $timestamp = $timestamp->getTimestamp();
+                    break;
 
-				default:
-				    if (strpos($dateformat,'DATE_') === 0)
+                default:
+                    if (strpos($dateformat, 'DATE_') === 0)
                         $dateformat = constant($dateformat);
 
-				    $timestamp = $timestamp->format($dateformat);
-					break;
-			}
+                    $timestamp = $timestamp->format($dateformat);
+                    break;
+            }
 
 
             $log =
-			[
-				'timestamp'     => $timestamp,
-				'environment'   => $matches[2],
-				'level'         => $matches[3],
-				'message'       => trim($matches[4]),
-				'data'          => $structs ? null : json_encode($structs),
-			];
+                [
+                    'timestamp'   => $timestamp,
+                    'environment' => $matches[2],
+                    'level'       => $matches[3],
+                    'message'     => trim($matches[4]),
+                    'data'        => $structs ? null : json_encode($structs),
+                ];
 
 
-            $fnSafeDecode = function($str) {
+            $fnSafeDecode = function ($str) {
                 $des = json_decode($str, true);
                 return JSON_ERROR_NONE === json_last_error() ? $des : null;
             };
@@ -114,8 +108,7 @@ class Model_LogParser
             /**
              * Deserialize data and add it to params.
              */
-            if (!empty($structs) && $smart_serialization)
-            {
+            if (!empty($structs) && $smart_serialization) {
                 if (count($structs) === 1) {
                     $log['params'] = $fnSafeDecode($structs[0]);
                 } else {
@@ -134,6 +127,57 @@ class Model_LogParser
         }
 
         return false;
+    }
+
+
+    /**
+     * Extract JSON strings from a string.
+     *
+     * This function is faster than use regular expressions.
+     *
+     * @param string $str
+     * @return array<array{start:int,length:int,string:string}>
+     */
+    protected static function jsonExtract(string $str): array
+    {
+        $numBrackets = $initStr = $foundAt = -1;
+        $jsonStrings = [];
+
+        foreach (str_split($str) as $k => $chr) {
+            switch ($chr) {
+                case '{':
+                    $numBrackets++;
+
+                    if ($initStr === -1) {
+                        $initStr = $k;
+                    }
+                    break;
+
+                case '}':
+                    if ($initStr >= 0) {
+                        $numBrackets--;
+
+                        if ($numBrackets === -1 && $foundAt > -1) {
+                            $jsonStrings[] = [
+                                'start'  => $foundAt,
+                                'length' => $k - $foundAt + 1,
+                                'string' => substr($str, $foundAt, $k - $foundAt + 1),
+                            ];
+
+                            $numBrackets   = $initStr = $foundAt = -1;
+                        }
+                    }
+                    break;
+
+                case '"':
+                    if ($numBrackets > -1 && $foundAt === -1) {
+                        $foundAt = $initStr;
+                    }
+                    break;
+            }
+        }
+
+        return $jsonStrings;
     }
 
 
--- src/Config/Version.php
+++ src/Config/Version.php
@@ -2,6 +2,6 @@
  return array (
   'major' => 4,
   'minor' => 1,
-  'build' => 63,
+  'build' => 66,
   'codename' => '',
 );
